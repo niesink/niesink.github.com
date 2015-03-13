2c9d096d4ebb447f3ac94975ea6cfa68dd585080
Luke Niesink <luke.niesink@topicus.nl>
2015-03-13 12:00:12 +0100

    klikbare notificaties

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
+  var title = 'Hi!';
+  var body = 'Een nieuw bericht!';
   var icon = '/gietnaad_logo.png';
   var tag = 'simple-push-demo-notification-tag';
 
@@ -14,3 +14,28 @@ self.addEventListener('push', function(event) {
     })
   );
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients.matchAll({
+      type: "window"
+    })
+    .then(function(clientList) {
+      for (var i = 0; i &lt; clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('/');
+        }
+      })
+    );
+  });
