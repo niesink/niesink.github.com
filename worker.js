ba6be4ab430d0fff993c94776660c7ada984c09d
Luke Niesink <luke.niesink@topicus.nl>
2015-03-13 10:31:52 +0100

    initial push test

--- worker.js
+++ worker.js
@@ -1,16 +1,16 @@
-// The SW will be shutdown when not in use to save memory,
-// be aware that any global state is likely to disappear
-console.log("SW startup");
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
 
-self.addEventListener('install', function(event) {
-  console.log("SW installed");
-});
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = '/gietnaad_logo.png';
+  var tag = 'simple-push-demo-notification-tag';
 
-self.addEventListener('activate', function(event) {
-  console.log("SW activated");
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log("Caught a fetch!", event.request);
-  //event.respondWith(new Response("Hello world!"));
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag
+    })
+  );
 });
